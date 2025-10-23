c68298a3792d9dbf978d88280c697a2fb0cb54ff
yosifivanov08 <kirmiziagac0@gmail.com>
2025-10-23 15:53:34 +0300

    Added PlayerMove Method

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tictactoe {
     
@@ -19,9 +20,97 @@ public class Tictactoe {
         System.out.println("| " + box.get(6) + " | " + box.get(7) + " | " + box.get(8) + " | ");
         System.out.println("-------------");
     }
+    static void playerMove(){
+        System.out.print("Enter Your Move(1-9): ");
+        Scanner scanner = new Scanner(System.in);
+        int playerMove = scanner.nextInt() - 1; 
+            switch(playerMove){
+                default:
+                    System.out.print("Please Enter A Valid Choice(1-9): ");
+                    playerMove = scanner.nextInt() - 1;
+                    break;
+                case 0:
+                    if(box.get(0) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 1:
+                    if(box.get(1) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 2:
+                    if(box.get(2) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 3:
+                    if(box.get(3) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 4:
+                    if(box.get(4) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 5:
+                    if(box.get(5) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 6:
+                    if(box.get(6) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 7:
+                    if(box.get(7) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                case 8:
+                    if(box.get(8) == ' '){
+                        box.set(playerMove, 'X');
+                    } else{
+                        System.out.print("Please Enter A Valid Choice(1-9): ");
+                        playerMove = scanner.nextInt() - 1;
+                        break;
+                    }
+                
+            }
+        
+    }
     public static void main(String[] args){
         createBox();
         showMap();
+        playerMove();
+        showMap();
+
         
     }
 }
