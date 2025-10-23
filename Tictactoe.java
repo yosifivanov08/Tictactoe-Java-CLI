eeea2f3115d04c210ce284c9d9fe7c8d8f1f17dc
yosifivanov08 <kirmiziagac0@gmail.com>
2025-10-23 21:16:07 +0300

    computerMove method simplified

--- Tictactoe.java
+++ Tictactoe.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
 
 public class Tictactoe {
     
-    static Scanner scanner = new Scanner(System.in);
-    static ArrayList<Character>box = new ArrayList<>();
-    static String winner = " ";
+    static Scanner scanner = new Scanner(System.in);  // Declaring input 
+    static ArrayList<Character>box = new ArrayList<>(); // Declaring array for map
+    static String winner = " "; // Declaring a global string to use in deciding the winner
     
-    static void clearScreen(){
+    // Clears the terminal
+    static void clearScreen(){ 
         System.out.print("\033[H\033[2J");  
         System.out.flush();
     }
 
-    static void createBox(){
+    // Creates the map
+    static void createBox(){  
             for(int i=0; i<9; i++){
                 box.add(' ');
             }
             
     }
-    static void showMap(){
+    // Shows the current map
+    static void showMap(){ 
         System.out.println("-------------");
         System.out.println("| " + box.get(0) + " | " + box.get(1) + " | " + box.get(2) + " | ");
         System.out.println("-------------");
@@ -27,6 +30,7 @@ public class Tictactoe {
         System.out.println("| " + box.get(6) + " | " + box.get(7) + " | " + box.get(8) + " | ");
         System.out.println("-------------");
     }
+    // Player inputs his move
     static void playerMove(){
         System.out.print("Enter Your Move: ");
         
@@ -39,121 +43,74 @@ public class Tictactoe {
        
 
     }
+    // Computer inputs his move
     static void computerMove(){
         if(box.get(0) != ' ' && box.get(1) != ' ' && box.get(2) != ' ' && box.get(3) != ' ' && box.get(4) != ' ' &&box.get(5) != ' ' && box.get(6) != ' ' && box.get(7) != ' ' && box.get(8) != ' '){
             return;
         }
         int computerNumber = (int)(Math.random() * 9);
-        // System.out.println(computerNumber);
-            switch(computerNumber){
-                case 0:
-                    if(box.get(0) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 1:
-                    if(box.get(1) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 2:
-                    if(box.get(2) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 3:
-                    if(box.get(3) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 4:
-                    if(box.get(4) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 5:
-                    if(box.get(5) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 6:
-                    if(box.get(6) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 7:
-                    if(box.get(7) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                case 8:
-                    if(box.get(8) == ' '){
-                        box.set(computerNumber, 'O');
-                    } else {
-                        computerMove();
-                    } break;
-                
+        
+            if(box.get(computerNumber) != ' '){
+                computerMove();
+            } else{
+                box.set(computerNumber, 'O');
             }
     
 
     }
+    // Deciding winner
     static void decideWinner(){
-        
-        if(box.get(0) == 'X' && box.get(1) == 'X' && box.get(2) == 'X'){ // first row player
+        // Situations where the winner is player
+        if(box.get(0) == 'X' && box.get(1) == 'X' && box.get(2) == 'X'){ // first row 
             winner = "Player";
         }
-        if(box.get(3) == 'X' && box.get(4) == 'X' && box.get(5) == 'X'){ // second row player
+        if(box.get(3) == 'X' && box.get(4) == 'X' && box.get(5) == 'X'){ // second row 
             winner = "Player";
         }
-        if(box.get(6) == 'X' && box.get(7) == 'X' && box.get(8) == 'X'){ // third row player
+        if(box.get(6) == 'X' && box.get(7) == 'X' && box.get(8) == 'X'){ // third row 
             winner = "Player";
         }
-        if(box.get(0) == 'X' && box.get(3) == 'X' && box.get(6) == 'X'){ // first vertical player
+        if(box.get(0) == 'X' && box.get(3) == 'X' && box.get(6) == 'X'){ // first vertical 
             winner = "Player";
         }
-        if(box.get(1) == 'X' && box.get(4) == 'X' && box.get(7) == 'X'){ // second vertical player
+        if(box.get(1) == 'X' && box.get(4) == 'X' && box.get(7) == 'X'){ // second vertical 
             winner = "Player";
         }
-        if(box.get(2) == 'X' && box.get(5) == 'X' && box.get(8) == 'X'){ // third vertical player
+        if(box.get(2) == 'X' && box.get(5) == 'X' && box.get(8) == 'X'){ // third vertical 
             winner = "Player";
         }
-        // computer 
+        // Situations where the winner is computer 
 
-        if(box.get(0) == 'O' && box.get(1) == 'O' && box.get(2) == 'O'){ // first row computer
+        if(box.get(0) == 'O' && box.get(1) == 'O' && box.get(2) == 'O'){ // first row 
             winner = "Computer";
         }
-        if(box.get(3) == 'O' && box.get(4) == 'O' && box.get(5) == 'O'){ // second row computer
+        if(box.get(3) == 'O' && box.get(4) == 'O' && box.get(5) == 'O'){ // second row 
             winner = "Computer";
         }
-        if(box.get(6) == 'O' && box.get(7) == 'O' && box.get(8) == 'O'){ // thirt row coomputer
+        if(box.get(6) == 'O' && box.get(7) == 'O' && box.get(8) == 'O'){ // thirt row coompute
             winner = "Computer";
         }
-        if(box.get(0) == 'O' && box.get(3) == 'O' && box.get(6) == 'O'){ // first vertical player
+        if(box.get(0) == 'O' && box.get(3) == 'O' && box.get(6) == 'O'){ // first vertical 
             winner = "Computer";
         }
-        if(box.get(1) == 'O' && box.get(4) == 'O' && box.get(7) == 'O'){ // second vertical player
+        if(box.get(1) == 'O' && box.get(4) == 'O' && box.get(7) == 'O'){ // second verticalplayer
             winner = "Computer";
         }
-        if(box.get(2) == 'O' && box.get(5) == 'O' && box.get(8) == 'O'){ // left diagonal player
+        if(box.get(2) == 'O' && box.get(5) == 'O' && box.get(8) == 'O'){ // left diagonal 
             winner = "Computer";
         }
+
         if(winner == "Player"){
-            System.out.println("PLayer Won!");
+            System.out.println("Player Won!");
             return;
         } if(winner == "Computer"){
             System.out.println("Computer Won!");
             return;
-        } if(box.get(0) != ' ' && box.get(1) != ' ' && box.get(2) != ' ' && box.get(3) != ' ' && box.get(4) != ' ' &&box.get(5) != ' ' && box.get(6) != ' ' && box.get(7) != ' ' && box.get(8) != ' '){
+        } 
+        // If all the boxes are filled
+        if(box.get(0) != ' ' && box.get(1) != ' ' && box.get(2) != ' ' && box.get(3) != ' ' && box.get(4) != ' ' &&box.get(5) != ' ' && box.get(6) != ' ' && box.get(7) != ' ' && box.get(8) != ' '){
             if(winner == "Player"){
-                System.out.println("PLayer Won!");
+                System.out.println("Player Won!");
                 return;
             } else if(winner == "Computer"){
 
